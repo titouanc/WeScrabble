baf06a0dd3498162cf62616ee0952b962d7aa197
iTitou <moiandme@gmail.com>
2016-06-02 00:13:56 +0200

    [add] Better muiltithreading, letters frequency, etc...

--- assets/atlib/wescrabble/runtests.sh
+++ assets/atlib/wescrabble/runtests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 IAT=./iat
+
+selector='*'
+[[ -z "$1" ]] || selector=$1
+
 sedcmd='s/tests\/(.+)\.at/~.tests.\1.runTest(~.tests.reporter());/'
-find tests/Test*.at | sed -E $sedcmd | ${IAT} -q | grep -Ev '^nil$'
+find tests/Test$selector.at | sed -E $sedcmd | ${IAT} -q | grep -Ev '^nil$'
