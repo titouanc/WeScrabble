b9137bffe9f87ddfb7725a0ae0ff98edfad868a4
iTitou <moiandme@gmail.com>
2016-06-01 13:55:35 +0200

    [add] TamTam + test collection

--- assets/atlib/wescrabble/runtests.sh
+++ assets/atlib/wescrabble/runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IAT=./iat
-
-$IAT -p tests/runTests.at
+sedcmd='s/tests\/(.+)\.at/~.tests.\1.runTest(~.tests.reporter());/'
+find tests/Test*.at | sed -E $sedcmd | ${IAT} -q | grep -Ev '^nil$'
